4fce2e9cbf366757bc914cf0609699b858385dfd
AkaneRuan <xiqiruan@gmail.com>
2018-08-23 17:17:41 -0700

    Initialise

---
+++ src/main/java/akane/springframework/sf_petclinic/SfPetclinicApplication.java
@@ -0,0 +1,12 @@
+package akane.springframework.sf_petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfPetclinicApplication.class, args);
+    }
+}
